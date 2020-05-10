beb6c293ddf36631df7b54644b72f5c46b47354e
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2020-05-10 19:20:35 +0800

    Update 1001_javaBasics.java

--- 1001_javaBasics.java
+++ 1001_javaBasics.java
@@ -1,1 +1,7 @@
 
+public class Main {
+  public static void main(String[] args) {
+    boolean hasHighIncome = true;
+    boolean hasGoodCredit = true;
+    boolean hasCriminalRecord = false;
+    boolean isEligible = hasHighIncome || hasGoodCredit !&& hasCriminalRecord;
